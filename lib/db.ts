daab049ea5c5b49c5630fa3d784a62fb530712ef
vivek-codepalette <vivek@codepalette.ai>
2025-01-12 17:52:06 +0700

    initial commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,3 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient, ServerApiVersion } from "mongodb"
  
 if (!process.env.MONGODB_URI) {
@@ -17,9 +16,7 @@ const options = {
 let client: MongoClient
  
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClient?: MongoClient
   }
  
@@ -28,10 +25,7 @@ if (process.env.NODE_ENV === "development") {
   }
   client = globalWithMongo._mongoClient
 } else {
-  // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
 }
- 
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
+
 export default client
--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import { SignupValidation } from '@/lib/validations/user';
 import type { SignupValidationType } from '@/lib/validations/user';
 
 const userSchema = new mongoose.Schema<SignupValidationType>({
